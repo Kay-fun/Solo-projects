ec6bb03b20e91fc6930c652e0b015bb76aa297d7
Kay-fun <55129304+Kay-fun@users.noreply.github.com>
2020-09-04 17:12:26 +0200

    Add files via upload

---
+++ projet_PHP/visitor.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Web PHP</title>
+    </head>
+
+    <body>
+        <h1>Web page with PHP</h1>
+        <p>Insertion de PHP <br>
+
+        Petits tests :
+        <?php echo "Display by PHP output"; ?>
+        </p>
+
+        <ul>
+            <li style="color: blue;">Texte en bleu</li>
+            <li style="color: red;">Texte en rouge</li>
+            <li style="color: green;">Texte en vert</li>
+        </ul>
+    </body>
+</html>
